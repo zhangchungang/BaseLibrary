f7d06265e6e0f7884f6470b60d822acc1f44af1f
zhangchungang <zhangchungang@126.com>
2017-02-22 09:37:58 +0800

    修改滚动页控件

--- base-library/app/src/main/java/com/dome/activity/MainActivity.java
+++ base-library/app/src/main/java/com/dome/activity/MainActivity.java
@@ -2,12 +2,22 @@ package com.dome.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.library.activity.AppTitleBarActivity;
+import com.library.views.ScrollPageView;
+
+import org.w3c.dom.Text;
+
+import butterknife.Bind;
 
 public class MainActivity extends AppTitleBarActivity {
 
+    @Bind(R.id.mScrollPageView)
+    ScrollPageView mScrollPageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,25 @@ public class MainActivity extends AppTitleBarActivity {
                 finish();
             }
         });
+        TextView tv01 = new TextView(this);
+        tv01.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        tv01.setGravity(Gravity.CENTER);
+        tv01.setText("01");
+        TextView tv02 = new TextView(this);
+        tv02.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        tv02.setGravity(Gravity.CENTER);
+        tv02.setText("02");
+        TextView tv03 = new TextView(this);
+        tv03.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        tv03.setGravity(Gravity.CENTER);
+        tv03.setText("03");
+        View[] v = {tv01,tv02,tv03};
+        mScrollPageView.setViews(v);
     }
 }
